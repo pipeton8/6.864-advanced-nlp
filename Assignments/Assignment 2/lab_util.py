73fdbbcfe1c886a3591c44e6c09aaabc19dcf15e
Felipe Del Canto <felipe.delcanto@icloud.com>
2021-10-08 16:33:34 -0400

    Corrected HW2 auxiliary files

--- Assignments/Assignment 2/lab_util.py
+++ Assignments/Assignment 2/lab_util.py
@@ -1,16 +1,107 @@
---2021-10-08 20:20:37--  https://github.com/mit-6864/hw2/raw/main/lab_util.py
-Resolving github.com (github.com)... 140.82.112.3
-Connecting to github.com (github.com)|140.82.112.3|:443... connected.
-HTTP request sent, awaiting response... 302 Found
-Location: https://raw.githubusercontent.com/mit-6864/hw2/main/lab_util.py [following]
---2021-10-08 20:20:37--  https://raw.githubusercontent.com/mit-6864/hw2/main/lab_util.py
-Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.108.133, 185.199.109.133, 185.199.110.133, ...
-Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.108.133|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: 3462 (3.4K) [text/plain]
-Saving to: ‘lab_util.py.3’
+import numpy as np
+import re
+import string
+import random
 
-     0K ...                                                   100% 44.6M=0s
+def show_similar_words(tokenizer, reps, tokens):
+    reps = reps / (np.sqrt((reps ** 2).sum(axis=1, keepdims=True)))
+    #for i, (word, token) in enumerate(tokenizer.word_to_token.items()):
+    for token in tokens:
+        word = tokenizer.token_to_word[token]
+        rep = reps[token, :]
+        sims = ((reps - rep) ** 2).sum(axis=1)
+        nearest = np.argsort(sims)
+        print(word, token)
+        for j in nearest[1:6]:
+            print(" ", tokenizer.token_to_word[j], "%.3f" % sims[j])
 
-2021-10-08 20:20:38 (44.6 MB/s) - ‘lab_util.py.3’ saved [3462/3462]
+class Tokenizer:
+  def __init__(self, min_occur=10):
+    self.word_to_token = {}
+    self.token_to_word = {}
+    self.word_count = {}
 
+    self.word_to_token['<unk>'] = 0
+    self.token_to_word[0] = '<unk>'
+    self.vocab_size = 1
+
+    self.min_occur = min_occur
+
+  def fit(self, corpus):
+    for review in corpus:
+      review = review.strip().lower()
+      words = re.findall(r"[\w']+|[.,!?;]", review)
+      for word in words:
+          if word not in self.word_count:
+              self.word_count[word] = 0
+          self.word_count[word] += 1
+
+    for review in corpus:
+      review = review.strip().lower()
+      words = re.findall(r"[\w']+|[.,!?;]", review)
+      for word in words:
+        if self.word_count[word] < self.min_occur:
+          continue
+        if word in self.word_to_token:
+          continue
+        self.word_to_token[word] = self.vocab_size
+        self.token_to_word[self.vocab_size] = word
+        self.vocab_size += 1
+
+  def tokenize(self, corpus):
+    tokenized_corpus = []
+    for review in corpus:
+      review = review.strip().lower()
+      words = re.findall(r"[\w']+|[.,!?;]", review)
+      tokenized_review = []
+      for word in words:
+        if word not in self.word_to_token:
+          tokenized_review.append(0)
+        else:
+          tokenized_review.append(self.word_to_token[word])
+      tokenized_corpus.append(tokenized_review)
+    return tokenized_corpus
+
+  def de_tokenize(self, tokenized_corpus):
+    corpus = []
+    for tokenized_review in tokenized_corpus:
+      review = []
+      for token in tokenized_review:
+        review.append(self.token_to_word[token])
+      corpus.append(" ".join(review))
+    return corpus
+
+
+class CountVectorizer:
+  def __init__(self, min_occur=10):
+    self.tokenizer = Tokenizer(min_occur)
+
+  def fit(self, corpus):
+    self.tokenizer.fit(corpus)
+
+  def transform(self, corpus):
+    n = len(corpus)
+    X = np.zeros((n, self.tokenizer.vocab_size))
+    for i, review in enumerate(corpus):
+      review = review.strip().lower()
+      words = re.findall(r"[\w']+|[.,!?;]", review)
+      for word in words:
+        if word not in self.tokenizer.word_count or self.tokenizer.word_count[word] < self.tokenizer.min_occur:
+          X[i][0] += 1
+        else:
+          X[i][self.tokenizer.word_to_token[word]] += 1
+    return X
+
+def get_ngrams(tokenized_corpus, window_size, pad_idx=2006):
+    ngrams = []
+    for i, review in enumerate(tokenized_corpus):
+        for j, word in enumerate(review):
+            min_ind = max(0, j-window_size)
+            max_ind = min(len(review), j+window_size+1)
+            ctx = np.zeros(2 * window_size, dtype=np.int64) + pad_idx
+            for ik, k in enumerate(range(min_ind, j)):
+                ctx[ik] = review[k]
+            for ik, k in enumerate(range(j+1, max_ind)):
+                ctx[window_size+ik] = review[k]
+            ngrams.append((ctx, review[j]))
+    return ngrams
