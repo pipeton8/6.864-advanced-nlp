02fee0af63b91c1c50606f06d6906173c6f35c5e
Felipe Del Canto <felipe.delcanto@icloud.com>
2021-10-08 16:23:00 -0400

    Added HW2 auxiliary files

---
+++ Assignments/Assignment 2/lab_util.py
@@ -0,0 +1,16 @@
+--2021-10-08 20:20:37--  https://github.com/mit-6864/hw2/raw/main/lab_util.py
+Resolving github.com (github.com)... 140.82.112.3
+Connecting to github.com (github.com)|140.82.112.3|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://raw.githubusercontent.com/mit-6864/hw2/main/lab_util.py [following]
+--2021-10-08 20:20:37--  https://raw.githubusercontent.com/mit-6864/hw2/main/lab_util.py
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.108.133, 185.199.109.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.108.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3462 (3.4K) [text/plain]
+Saving to: ‘lab_util.py.3’
+
+     0K ...                                                   100% 44.6M=0s
+
+2021-10-08 20:20:38 (44.6 MB/s) - ‘lab_util.py.3’ saved [3462/3462]
+
